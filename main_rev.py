35db668ba1d97479b372910b645fec8b824b7a6b
devdav <tdav06@yahoo.com>
2023-08-02 00:21:51 -0400

    added comment

--- main_rev.py
+++ main_rev.py
@@ -3,7 +3,10 @@ import re
 def validate_card(array):
     for idx in range(array[0]):
         card_num = array[idx+1]
-        if re.search("^[456]([0-9]{14})|^[456][0-9]{3}(-[0-9]{4}){3}$",card_num) and \
+        # first exp checks first digit is 4, 5 or 6, checks if length is 16 digits or
+        # number is four sets of four divided by hyphen.
+        # second exp check if any digits repeat four or more times. 
+        if re.search("^[456]([0-9]{15}$)|^[456][0-9]{3}(-[0-9]{4}){3}$",card_num) and \
             not re.search(r"(\d)\1{3,}",re.sub("\-","",card_num)):
             print("Valid",card_num)
             continue
